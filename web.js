daf8be49f1014448eb727d7fa0aada96e193deb0
Frank <frank.junheng@gmail.com>
2014-06-27 13:15:38 -0700

    Commit

--- web.js
+++ web.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var http = require('http');
 var gzippo = require('gzippo');
-var logger = require('morgan');
-
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 var app = express();
-app.use(logger());
+app.use(morgan('dev'));
+app.use(bodyParser());
+app.use(methodOverride());
 app.use(gzippo.staticGzip('' + __dirname));
-
 var server = http.createServer(app);
 server.listen(process.env.PORT || 5000);
